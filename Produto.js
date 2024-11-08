98c0f50a5900a1e2185570a88098ab2bba8b1c7c
MMarcooss <marquinhostavares03@gmail.com>
2024-11-08 20:11:55 -0300

    Adicionado página produtos.

---
+++ Produto.js
@@ -0,0 +1,3 @@
+const URL = "https://rdm2alro.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22produto%22%5D%7B%0A++titulo_prod%2C%0A++%22imagem_produto%22%3A+imagem_produto.asset-%3Eurl%0A%7D";
+
+addEventListener("load")
